52174f8d9cc1a8d483f9dedf0edc0c57aa4ea9be
jmuell <jmuell.dev@gmail.com>
2022-02-01 11:30:25 +0100

    Fix package imports

--- endpoint/http/src/test/java/de/fraunhofer/iosb/ilt/faaast/service/endpoint/http/MappingManagerTest.java
+++ endpoint/http/src/test/java/de/fraunhofer/iosb/ilt/faaast/service/endpoint/http/MappingManagerTest.java
@@ -17,8 +17,6 @@ package de.fraunhofer.iosb.ilt.faaast.service.endpoint.http;
 import de.fraunhofer.iosb.ilt.faaast.service.endpoint.http.http.HttpMethod;
 import de.fraunhofer.iosb.ilt.faaast.service.endpoint.http.http.HttpRequest;
 import de.fraunhofer.iosb.ilt.faaast.service.endpoint.http.serialization.HttpJsonSerializer;
-import de.fraunhofer.iosb.ilt.faaast.service.endpoint.http.utils.ElementPathUtils;
-import de.fraunhofer.iosb.ilt.faaast.service.endpoint.http.utils.EncodingUtils;
 import de.fraunhofer.iosb.ilt.faaast.service.model.AASFull;
 import de.fraunhofer.iosb.ilt.faaast.service.model.v3.api.Content;
 import de.fraunhofer.iosb.ilt.faaast.service.model.v3.api.Level;
