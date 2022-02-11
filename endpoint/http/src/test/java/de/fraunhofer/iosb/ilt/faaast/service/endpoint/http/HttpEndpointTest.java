06aaf7a2f4842a73d7fc8c2725d114c19cc45078
jmuell <jmuell.dev@gmail.com>
2022-02-11 16:00:57 +0100

    Update HttpEndpointTest.java

--- endpoint/http/src/test/java/de/fraunhofer/iosb/ilt/faaast/service/endpoint/http/HttpEndpointTest.java
+++ endpoint/http/src/test/java/de/fraunhofer/iosb/ilt/faaast/service/endpoint/http/HttpEndpointTest.java
@@ -43,7 +43,6 @@ import org.eclipse.jetty.http.HttpStatus;
 import org.junit.AfterClass;
 import org.junit.Assert;
 import org.junit.BeforeClass;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
