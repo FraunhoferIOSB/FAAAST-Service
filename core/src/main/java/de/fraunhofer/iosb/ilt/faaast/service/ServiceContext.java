bcb415ae751c4e79665410d91951430549f05438
Michael Jacoby <michael.jacoby@iosb.fraunhofer.de>
2022-02-23 09:39:54 +0100

    javadoc core

--- core/src/main/java/de/fraunhofer/iosb/ilt/faaast/service/ServiceContext.java
+++ core/src/main/java/de/fraunhofer/iosb/ilt/faaast/service/ServiceContext.java
@@ -23,11 +23,30 @@ import io.adminshell.aas.v3.model.OperationVariable;
 import io.adminshell.aas.v3.model.Reference;
 
 
+/**
+ * Abstraction of Service to be used in other components to limit their access
+ * to the service.
+ */
 public interface ServiceContext {
 
+    /**
+     * Provides type information about an element identified by reference.
+     *
+     * @param reference reference identifying the element
+     * @return type information of the referenced element, empty
+     *         {@link de.fraunhofer.iosb.ilt.faaast.service.typing.ContainerTypeInfo} if
+     *         no matching type is found, null if reference is null
+     */
     public TypeInfo getTypeInfo(Reference reference);
 
 
+    /**
+     * Executes a request
+     *
+     * @param request request to execute
+     * @return result of executing the request
+     * @throws IllegalArgumentException if request is null
+     */
     public Response execute(Request request);
 
 
@@ -37,12 +56,28 @@ public interface ServiceContext {
      *
      * @return a deep copied AssetAdministrationShellEnvironment instance of the
      *         service
+     * @throws java.lang.Exception
      */
     public AssetAdministrationShellEnvironment getAASEnvironment() throws Exception;
 
 
-    public MessageBus<?> getMessageBus();
+    /**
+     * Returns the message bus of the service
+     *
+     * @return the message bus of the service
+     */
+    public MessageBus getMessageBus();
 
 
+    /**
+     * Returns the output variables of an operation identified by a reference
+     *
+     * @param reference the reference identifying the operation
+     * @return output variables of the operation identified by the reference
+     * @throws IllegalArgumentException if reference is null
+     * @throws IllegalArgumentException if reference cannot be resolved
+     * @throws IllegalArgumentException if reference does not point to an
+     *             operation
+     */
     public OperationVariable[] getOperationOutputVariables(Reference reference);
 }
