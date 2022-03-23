b41f7632d7266c575b07d850c4941826c1221512
Michael Jacoby <michael.jacoby@iosb.fraunhofer.de>
2022-03-23 12:45:26 +0100

    bugfix for enum serialization

---
+++ core/src/main/java/de/fraunhofer/iosb/ilt/faaast/service/util/ReflectionHelper.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright (c) 2021 Fraunhofer IOSB, eine rechtlich nicht selbstaendige
+ * Einrichtung der Fraunhofer-Gesellschaft zur Foerderung der angewandten
+ * Forschung e.V.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package de.fraunhofer.iosb.ilt.faaast.service.util;
+
+import io.github.classgraph.ClassGraph;
+import java.util.List;
+
+
+/**
+ * Helper for tasks related to reflection
+ */
+public class ReflectionHelper {
+
+    private static final String ROOT_PACKAGE_NAME = "de.fraunhofer.iosb.ilt.faaast.service";
+    private static final String MODEL_PACKAGE_NAME = ROOT_PACKAGE_NAME + ".model";
+
+    /**
+     * List of enum classes that are part of the FA³ST model.
+     */
+    public static final List<Class<Enum>> ENUMS;
+
+    static {
+        ENUMS = new ClassGraph()
+                .enableClassInfo()
+                .acceptPackages(MODEL_PACKAGE_NAME)
+                .scan()
+                .getAllEnums()
+                .loadClasses(Enum.class);
+    }
+}
