1b295d65c51de5066cee1fa2b9b05fd144a77068
Michael Jacoby <michael.jacoby@iosb.fraunhofer.de>
2022-07-26 14:30:14 +0200

    improved typesafety, fixed potentials crash when model is initialized with empty string as values

--- model/src/main/java/de/fraunhofer/iosb/ilt/faaast/service/model/value/primitive/ByteValue.java
+++ model/src/main/java/de/fraunhofer/iosb/ilt/faaast/service/model/value/primitive/ByteValue.java
@@ -14,6 +14,9 @@
  */
 package de.fraunhofer.iosb.ilt.faaast.service.model.value.primitive;
 
+import org.apache.commons.lang3.StringUtils;
+
+
 public class ByteValue extends TypedValue<Byte> {
 
     public ByteValue() {
@@ -34,7 +37,7 @@ public class ByteValue extends TypedValue<Byte> {
 
     @Override
     public void fromString(String value) throws ValueFormatException {
-        if (value == null) {
+        if (StringUtils.isAllBlank(value)) {
             this.setValue(null);
             return;
         }
