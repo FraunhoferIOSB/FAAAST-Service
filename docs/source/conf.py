f34b5a32d21b497dc2cae3c5529f6e920c2906c2
jmuell <jmuell.dev@gmail.com>
2022-08-09 10:52:45 +0200

    Add custom css file

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -31,3 +31,12 @@ html_theme = 'sphinx_rtd_theme'
 
 # -- Options for EPUB output
 epub_show_urls = 'footnote'
+
+# These folders are copied to the documentation's HTML output
+html_static_path = ['_static']
+
+# These paths are either relative to html_static_path
+# or fully qualified paths (eg. https://...)
+html_css_files = [
+    'css/custom.css',
+]
