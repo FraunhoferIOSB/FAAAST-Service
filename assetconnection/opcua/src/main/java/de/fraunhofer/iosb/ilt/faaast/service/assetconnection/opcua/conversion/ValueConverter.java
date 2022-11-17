4eb561168e5a76fa9139411ce4536a2056b5807e
tbischoff2 <42869259+tbischoff2@users.noreply.github.com>
2022-11-17 14:42:41 +0100

    fix datetime conversions for opcua asset connection (#259)
    * fix datetime conversions for asset connection
    
    * fix if statement
    
    * added entry to changelog

--- assetconnection/opcua/src/main/java/de/fraunhofer/iosb/ilt/faaast/service/assetconnection/opcua/conversion/ValueConverter.java
+++ assetconnection/opcua/src/main/java/de/fraunhofer/iosb/ilt/faaast/service/assetconnection/opcua/conversion/ValueConverter.java
@@ -15,10 +15,13 @@
 package de.fraunhofer.iosb.ilt.faaast.service.assetconnection.opcua.conversion;
 
 import de.fraunhofer.iosb.ilt.faaast.service.model.value.primitive.Datatype;
+import de.fraunhofer.iosb.ilt.faaast.service.model.value.primitive.DateTimeValue;
 import de.fraunhofer.iosb.ilt.faaast.service.model.value.primitive.TypedValue;
 import de.fraunhofer.iosb.ilt.faaast.service.model.value.primitive.TypedValueFactory;
 import de.fraunhofer.iosb.ilt.faaast.service.model.value.primitive.ValueFormatException;
 import java.math.BigInteger;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
@@ -26,6 +29,7 @@ import java.util.Optional;
 import org.eclipse.milo.opcua.sdk.server.events.conversions.ImplicitConversions;
 import org.eclipse.milo.opcua.stack.core.BuiltinDataType;
 import org.eclipse.milo.opcua.stack.core.Identifiers;
+import org.eclipse.milo.opcua.stack.core.types.builtin.DateTime;
 import org.eclipse.milo.opcua.stack.core.types.builtin.NodeId;
 import org.eclipse.milo.opcua.stack.core.types.builtin.Variant;
 
@@ -80,8 +84,7 @@ public class ValueConverter {
      * @param value AAS value
      * @param targetType OPC UA target type
      * @return converted AAS value
-     * @throws ValueConversionException if value or targetType are null
-     * @throws ValueConversionException if conversion fails
+     * @throws ValueConversionException if value or targetType are null or conversion fails
      */
     public Variant convert(TypedValue<?> value, NodeId targetType) throws ValueConversionException {
         if (value == null) {
@@ -103,8 +106,7 @@ public class ValueConverter {
      * @param value OPC UAvalue
      * @param targetType AAS target type
      * @return converted OPC UA value
-     * @throws ValueConversionException if value or targetType are null
-     * @throws ValueConversionException if conversion fails
+     * @throws ValueConversionException if value or targetType are null or conversion fails
      */
     public TypedValue<?> convert(Variant value, Datatype targetType) throws ValueConversionException {
         if (value == null) {
@@ -135,7 +137,13 @@ public class ValueConverter {
             }
             BuiltinDataType builtinDataType = BuiltinDataType.fromNodeId(targetType);
             if (value.getValue() != null && Objects.equals(builtinDataType.getBackingClass(), value.getValue().getClass())) {
+                if ((value.getDataType() == Datatype.DATE_TIME) && (targetType == Identifiers.DateTime)) {
+                    return new Variant(new DateTime(((ZonedDateTime) value.getValue()).toInstant()));
+                }
                 return new Variant(value.getValue());
+            }
+            if ((value.getDataType() == Datatype.DATE_TIME) && (targetType.equals(Identifiers.DateTime))) {
+                return new Variant(new DateTime(((ZonedDateTime) value.getValue()).toInstant()));
             }
             return new Variant(ImplicitConversions.convert(value.getValue(), builtinDataType));
         }
@@ -144,7 +152,13 @@ public class ValueConverter {
         @Override
         public TypedValue<?> convert(Variant value, Datatype targetType) throws ValueConversionException {
             try {
-                return TypedValueFactory.create(targetType, value.getValue().toString());
+                if ((targetType == Datatype.DATE_TIME) && (value.getValue() instanceof DateTime)) {
+                    return TypedValueFactory.create(targetType,
+                            ZonedDateTime.ofInstant(((DateTime) value.getValue()).getJavaInstant(), ZoneId.of(DateTimeValue.DEFAULT_TIMEZONE)).toString());
+                }
+                else {
+                    return TypedValueFactory.create(targetType, value.getValue().toString());
+                }
             }
             catch (ValueFormatException e) {
                 throw new ValueConversionException(String.format("error converting value (value: %s, target datatype: %s",
