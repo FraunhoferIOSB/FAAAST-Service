32d8555b733415baab5282997f2e3436a9bb19d8
Michael Jacoby <michael.jacoby@iosb.fraunhofer.de>
2023-02-07 14:04:11 +0100

    update examples & documentation

--- persistence/memory/src/main/java/de/fraunhofer/iosb/ilt/faaast/service/persistence/memory/PersistenceInMemory.java
+++ persistence/memory/src/main/java/de/fraunhofer/iosb/ilt/faaast/service/persistence/memory/PersistenceInMemory.java
@@ -59,7 +59,6 @@ import org.apache.commons.lang3.StringUtils;
 /**
  * Implementation of {@link de.fraunhofer.iosb.ilt.faaast.service.persistence.Persistence} for in memory storage.
  *
- * <p>To decouple the AAS objects from the internal AAS Environment each returned object is copied deeply in advance.
  * Following types are not supported in the current version:
  * <ul>
  * <li>AASX packages
