3a906d0306ec072b31bb2c174f0c4f87ef77d645
Michael Jacoby <michael.jacoby@iosb.fraunhofer.de>
2023-05-23 16:44:50 +0200

    refactoring descriptor builder classes

--- model/src/main/java/de/fraunhofer/iosb/ilt/faaast/service/model/descriptor/impl/DefaultProtocolInformation.java
+++ model/src/main/java/de/fraunhofer/iosb/ilt/faaast/service/model/descriptor/impl/DefaultProtocolInformation.java
@@ -145,12 +145,14 @@ public class DefaultProtocolInformation implements ProtocolInformation {
     public abstract static class AbstractBuilder<T extends DefaultProtocolInformation, B extends AbstractBuilder<T, B>> extends ExtendableBuilder<T, B> {
 
         public B from(ProtocolInformation other) {
-            endpointAddress(other.getEndpointAddress());
-            endpointProtocol(other.getEndpointProtocol());
-            endpointProtocolVersion(other.getEndpointProtocolVersion());
-            subprotocol(other.getSubprotocol());
-            subprotocolBody(other.getSubprotocolBody());
-            subprotocolBodyEncoding(other.getSubprotocolBodyEncoding());
+            if (Objects.nonNull(other)) {
+                endpointAddress(other.getEndpointAddress());
+                endpointProtocol(other.getEndpointProtocol());
+                endpointProtocolVersion(other.getEndpointProtocolVersion());
+                subprotocol(other.getSubprotocol());
+                subprotocolBody(other.getSubprotocolBody());
+                subprotocolBodyEncoding(other.getSubprotocolBodyEncoding());
+            }
             return getSelf();
         }
 
