35c48c451597ea2d0e215021e32df0f482d6f376
cpathirana17 <c.pathirana79@gmail.com>
2023-06-18 22:56:29 +0200

    deleted unused directories and files.

--- scripts/generate_certificate.bat
+++
@@ -1,9 +0,0 @@
-
-set OPENSSL_CONF=C:\Program Files\OpenSSL-Win64
-
-set KEY_PATH=C:\Users\cha35985\IdeaProjects\FAAAST\certs\private.key
-set CERT_PATH=C:\Users\cha35985\IdeaProjects\FAAAST\certs\certificate.crt
-
-openssl req -x509 -newkey rsa:2048 -nodes -keyout %KEY_PATH% -out %CERT_PATH% -days 365 -subj "/CN=localhost"
-
-echo Private key and certificate generated successfully.
