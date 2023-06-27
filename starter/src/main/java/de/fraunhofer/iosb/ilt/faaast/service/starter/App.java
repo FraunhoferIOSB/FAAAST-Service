e946cc58558a95e075214e91d71f807530e22c0f
arabbitplays <osterjakob@gmai.com>
2023-06-27 13:36:01 +0200

    Clean up

--- starter/src/main/java/de/fraunhofer/iosb/ilt/faaast/service/starter/App.java
+++ starter/src/main/java/de/fraunhofer/iosb/ilt/faaast/service/starter/App.java
@@ -583,10 +583,11 @@ public class App implements Runnable {
 
     /**
      * Replaces separators in environment parameters to match the json format for the given config.
-     * 
+     *
      * @param config determines which separators have to be replaced
      *
      * @param configOverrides map of config overrides
+     *
      * @return map of config overrides that can be used in the json of the config
      */
     protected Map<String, String> removeSeparators(ServiceConfig config, Map<String, String> configOverrides) {
