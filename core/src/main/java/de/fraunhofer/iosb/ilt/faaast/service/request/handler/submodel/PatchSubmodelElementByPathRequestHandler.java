dab7d269a5d1f805b3af86995726b223e1ea6e6f
Michael Jacoby <michael.jacoby@iosb.fraunhofer.de>
2024-03-18 08:08:25 +0100

    HTTP endpoint now correctly returns status code 204 upon HTTP PATCH and no longer returns the updated element

--- core/src/main/java/de/fraunhofer/iosb/ilt/faaast/service/request/handler/submodel/PatchSubmodelElementByPathRequestHandler.java
+++ core/src/main/java/de/fraunhofer/iosb/ilt/faaast/service/request/handler/submodel/PatchSubmodelElementByPathRequestHandler.java
@@ -16,6 +16,7 @@ package de.fraunhofer.iosb.ilt.faaast.service.request.handler.submodel;
 
 import de.fraunhofer.iosb.ilt.faaast.service.assetconnection.AssetConnectionException;
 import de.fraunhofer.iosb.ilt.faaast.service.exception.MessageBusException;
+import de.fraunhofer.iosb.ilt.faaast.service.model.api.StatusCode;
 import de.fraunhofer.iosb.ilt.faaast.service.model.api.modifier.QueryModifier;
 import de.fraunhofer.iosb.ilt.faaast.service.model.api.request.submodel.PatchSubmodelElementByPathRequest;
 import de.fraunhofer.iosb.ilt.faaast.service.model.api.response.submodel.PatchSubmodelElementByPathResponse;
@@ -95,8 +96,7 @@ public class PatchSubmodelElementByPathRequestHandler extends AbstractSubmodelIn
                     .build());
         }
         return PatchSubmodelElementByPathResponse.builder()
-                .payload(newSubmodelElement)
-                .success()
+                .statusCode(StatusCode.SUCCESS_NO_CONTENT)
                 .build();
     }
 }
