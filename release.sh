3069ef2365997d9c95ba84c354df75d3b5836b32
Michael Jacoby <michael.jacoby@iosb.fraunhofer.de>
2024-12-10 16:31:02 +0100

    update release.sh

--- release.sh
+++ release.sh
@@ -65,6 +65,16 @@ function replaceVersion()
 	sed -r -z 's/(\x27de.fraunhofer.iosb.ilt.faaast.service:starter:)[^\x27]*\x27/\1'"${new_version}"'\x27/g' -i "$file"
 }
 
+# argument: newVersion
+function updateServiceProfileUrls()
+{
+	local new_version =$1
+	local major=$(echo "$new_version" | sed -E 's/^([0-9]+)\..*/\1/')
+	local minor=$(echo "$new_version" | sed -E 's/^[0-9]+\.([0-9]+)\..*/\1/')
+	local file="./model/src/main/java/de/fraunhofer/iosb/ilt/faaast/service/model/ServiceSpecificationProfile.java"
+	sed -i "s|https://github.com/FraunhoferIOSB/FAAAST-Service/API/[0-9]*/[0-9]*/|\0|https://github.com/FraunhoferIOSB/FAAAST-Service/API/$major/$minor/|g" "$file"	
+}
+
 
 echo "Releasing:  ${VERSION},
 tagged:    v${VERSION},
@@ -74,6 +84,9 @@ echo "Press enter to go"
 read -s
 
 echo "Replacing version numbers"
+updateServiceProfileUrls $NEXTVERSION
+echo "foo"
+read -s
 mvn -B versions:set -DgenerateBackupPoms=false -DnewVersion="${VERSION}"
 sed -i 's/<tag>HEAD<\/tag>/<tag>v'"${VERSION}"'<\/tag>/g' pom.xml
 replaceVersion "$README_FILE" "$VERSION"
@@ -106,6 +119,7 @@ replaceValue "$README_FILE" "$TAG_DOWNLOAD_SNAPSHOT" "$README_LATEST_SNAPSHOT_VE
 replaceValue "$INSTALLATION_FILE" "$TAG_DOWNLOAD_SNAPSHOT" "$INSTALLATION_LATEST_SNAPSHOT_VERSION_CONTENT"
 sed -i "2 i <!--start:${TAG_CHANGELOG_HEADER}-->\\n<!--end:${TAG_CHANGELOG_HEADER}-->" "$CHANGELOG_FILE"
 replaceValue "$CHANGELOG_FILE" "$TAG_CHANGELOG_HEADER" "## ${NEXTVERSION}-SNAPSHOT (current development version)"
+updateServiceProfileUrls $NEXTVERSION
 mvn -B spotless:apply
 
 echo "Git add ."
