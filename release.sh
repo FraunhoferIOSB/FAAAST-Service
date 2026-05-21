6b2303be0c73d67118a39a4944a8bf9a764438e1
carlos-schmidt <18703981+carlos-schmidt@users.noreply.github.com>
2026-05-21 11:22:23 +0200

    fix: update versions, release script

--- release.sh
+++ release.sh
@@ -13,6 +13,8 @@ TAG_DOWNLOAD_SNAPSHOT="download-snapshot"
 TAG_CHANGELOG_HEADER="changelog-header"
 CHANGELOG_FILE="./docs/source/other/release-notes.md"
 README_FILE="README.md"
+HELM_CHART_VALUES_FILE="charts/faaast-service/values.yaml"
+HELM_CHART_CHART_FILE="charts/faaast-service/Chart.yaml"
 INSTALLATION_FILE="./docs/source/basics/installation.md"
 README_LATEST_RELEASE_VERSION_CONTENT="[Download latest RELEASE version \($VERSION\)]\(https:\/\/repo1.maven.org\/maven2\/de\/fraunhofer\/iosb\/ilt\/faaast\/service\/starter\/${VERSION}\/starter-${VERSION}.jar\)"
 README_LATEST_SNAPSHOT_VERSION_CONTENT="[Download latest SNAPSHOT version \($NEXTVERSION\-SNAPSHOT)]\(https:\/\/purl.archive.org\/faaast\/service\/snapshot\/latest\)"
@@ -78,6 +80,21 @@ function updateServiceProfileUrls()
 	sed -i "s|https://github.com/FraunhoferIOSB/FAAAST-Service/API/[0-9]*\/[0-9]*\/|https://github.com/FraunhoferIOSB/FAAAST-Service/API/$major/$minor/|g" "$file"
 }
 
+# arguments: file, newTag
+function setHelmImageTag() {
+  local file=$1
+  local new_tag=$2
+  # assumes a single "tag:" line for the image in values.yaml
+  sed -i -E "s/^(\s*tag:\s*).*/\1${new_tag}/" "$file"
+}
+
+# arguments: file, newVersion
+function setHelmAppVersion() {
+  local file=$1
+  local new_version=$2
+  # matches: appVersion: "..."
+  sed -i -E 's/^(appVersion:\s*").*(")/\1'"${new_version}"'\2/' "$file"
+}
 
 echo "Releasing:  ${VERSION},
 tagged:    v${VERSION},
@@ -97,10 +114,12 @@ replaceValue "$INSTALLATION_FILE" "$TAG_DOWNLOAD_SNAPSHOT" ""
 replaceValue "$INSTALLATION_FILE" "$TAG_DOWNLOAD_RELEASE" "$INSTALLATION_LATEST_RELEASE_VERSION_CONTENT"
 replaceValue "$CHANGELOG_FILE" "$TAG_CHANGELOG_HEADER" "## ${VERSION}"
 removeTag "$CHANGELOG_FILE" "$TAG_CHANGELOG_HEADER"
+setHelmImageTag "$HELM_CHART_VALUES_FILE" "$VERSION"
+setHelmAppVersion "$HELM_CHART_CHART_FILE" "$VERSION"
 
 mvn -B spotless:apply
 
-echo "Updating thrid party license report"
+echo "Updating third party license report"
 mvn clean install license:aggregate-third-party-report -P build-ci -Dmaven.test.skip=false -B
 
 echo "Git add ."
@@ -111,7 +130,7 @@ read -s
 git commit -m "Release v${VERSION}"
 git tag -m "Release v${VERSION}" -a v"${VERSION}"
 
-echo "Next: replacing version nubmers [enter]"
+echo "Next: replacing version numbers [enter]"
 read -s
 mvn versions:set -DgenerateBackupPoms=false -DnewVersion="${NEXTVERSION}"-SNAPSHOT
 sed -i 's/<tag>v'"${VERSION}"'<\/tag>/<tag>'"${NEXTBRANCH}"'<\/tag>/g' pom.xml
@@ -119,7 +138,10 @@ replaceValue "$README_FILE" "$TAG_DOWNLOAD_SNAPSHOT" "$README_LATEST_SNAPSHOT_VE
 replaceValue "$INSTALLATION_FILE" "$TAG_DOWNLOAD_SNAPSHOT" "$INSTALLATION_LATEST_SNAPSHOT_VERSION_CONTENT"
 sed -i "2 i <!--start:${TAG_CHANGELOG_HEADER}-->\\n<!--end:${TAG_CHANGELOG_HEADER}-->" "$CHANGELOG_FILE"
 replaceValue "$CHANGELOG_FILE" "$TAG_CHANGELOG_HEADER" "## ${NEXTVERSION}-SNAPSHOT (current development version)"
-updateServiceProfileUrls $NEXTVERSION
+updateServiceProfileUrls "$NEXTVERSION"
+setHelmImageTag "$HELM_CHART_VALUES_FILE" "latest"
+setHelmAppVersion "$HELM_CHART_CHART_FILE" "$NEXTVERSION-SNAPSHOT"
+
 mvn -B spotless:apply
 
 echo "Git add ."
