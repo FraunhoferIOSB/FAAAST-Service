301d875ce302271e69f3c6ff4ef8cc925a7056d3
Tino Bischoff <tino.bischoff@iosb.fraunhofer.de>
2026-09-08 16:08:09 +0200

    update NodeSet

--- endpoint/opcua/src/main/java/de/fraunhofer/iosb/ilt/faaast/service/endpoint/opcua/ValueConverter.java
+++ endpoint/opcua/src/main/java/de/fraunhofer/iosb/ilt/faaast/service/endpoint/opcua/ValueConverter.java
@@ -69,7 +69,6 @@ import java.util.Optional;
 import opc.ua.aas.DataTypeIds;
 import opc.ua.aas.datatypes.AASAssetKind;
 import opc.ua.aas.datatypes.AASDataTypeIec61360;
-import opc.ua.aas.datatypes.AASDirection;
 import opc.ua.aas.datatypes.AASEntityEnumType;
 import opc.ua.aas.datatypes.AASHasKind;
 import opc.ua.aas.datatypes.AASKey;
@@ -78,7 +77,6 @@ import opc.ua.aas.datatypes.AASModellingKind;
 import opc.ua.aas.datatypes.AASQualifierKind;
 import opc.ua.aas.datatypes.AASReference;
 import opc.ua.aas.datatypes.AASReferenceTypes;
-import opc.ua.aas.datatypes.AASStateOfEvent;
 import opc.ua.aas.datatypes.AASSubmodelElements;
 import opc.ua.aas.datatypes.AASValueReferencePair;
 import org.eclipse.digitaltwin.aas4j.v3.model.AasSubmodelElements;
@@ -87,7 +85,6 @@ import org.eclipse.digitaltwin.aas4j.v3.model.AssetKind;
 import org.eclipse.digitaltwin.aas4j.v3.model.Blob;
 import org.eclipse.digitaltwin.aas4j.v3.model.DataTypeDefXsd;
 import org.eclipse.digitaltwin.aas4j.v3.model.DataTypeIec61360;
-import org.eclipse.digitaltwin.aas4j.v3.model.Direction;
 import org.eclipse.digitaltwin.aas4j.v3.model.Entity;
 import org.eclipse.digitaltwin.aas4j.v3.model.EntityType;
 import org.eclipse.digitaltwin.aas4j.v3.model.Key;
@@ -103,7 +100,6 @@ import org.eclipse.digitaltwin.aas4j.v3.model.Reference;
 import org.eclipse.digitaltwin.aas4j.v3.model.ReferenceElement;
 import org.eclipse.digitaltwin.aas4j.v3.model.ReferenceTypes;
 import org.eclipse.digitaltwin.aas4j.v3.model.RelationshipElement;
-import org.eclipse.digitaltwin.aas4j.v3.model.StateOfEvent;
 import org.eclipse.digitaltwin.aas4j.v3.model.SubmodelElement;
 import org.eclipse.digitaltwin.aas4j.v3.model.ValueList;
 import org.eclipse.digitaltwin.aas4j.v3.model.ValueReferencePair;
@@ -129,8 +125,8 @@ public class ValueConverter {
     private static final List<TypeMapper<ReferenceTypes, AASReferenceTypes>> REFERENCE_TYPES_LIST;
     private static final List<TypeMapper<EntityType, AASEntityEnumType>> ENTITY_TYPE_LIST;
     private static final List<TypeMapper<KeyTypes, AASKeyTypes>> KEY_ELEMENTS_LIST;
-    private static final List<TypeMapper<Direction, AASDirection>> DIRECTION_LIST;
-    private static final List<TypeMapper<StateOfEvent, AASStateOfEvent>> STATE_OF_EVENT_LIST;
+    //private static final List<TypeMapper<Direction, AASDirection>> DIRECTION_LIST;
+    //private static final List<TypeMapper<StateOfEvent, AASStateOfEvent>> STATE_OF_EVENT_LIST;
     private static final List<TypeMapper<AasSubmodelElements, AASSubmodelElements>> SUBMODEL_ELEMENTS_DATATYPE;
 
     private static class DatatypeMapper {
@@ -256,13 +252,13 @@ public class ValueConverter {
         KEY_ELEMENTS_LIST.add(new TypeMapper<>(KeyTypes.SUBMODEL_ELEMENT_COLLECTION, AASKeyTypes.of(AASKeyTypes.Options.SubmodelElementCollection)));
         KEY_ELEMENTS_LIST.add(new TypeMapper<>(KeyTypes.SUBMODEL_ELEMENT_LIST, AASKeyTypes.of(AASKeyTypes.Options.SubmodelElementList)));
 
-        DIRECTION_LIST = new ArrayList<>();
-        DIRECTION_LIST.add(new TypeMapper<>(Direction.INPUT, AASDirection.of(AASDirection.Options.input)));
-        DIRECTION_LIST.add(new TypeMapper<>(Direction.OUTPUT, AASDirection.of(AASDirection.Options.output)));
+        //DIRECTION_LIST = new ArrayList<>();
+        //DIRECTION_LIST.add(new TypeMapper<>(Direction.INPUT, AASDirection.of(AASDirection.Options.input)));
+        //DIRECTION_LIST.add(new TypeMapper<>(Direction.OUTPUT, AASDirection.of(AASDirection.Options.output)));
 
-        STATE_OF_EVENT_LIST = new ArrayList<>();
-        STATE_OF_EVENT_LIST.add(new TypeMapper<>(StateOfEvent.ON, AASStateOfEvent.of(AASStateOfEvent.Options.on)));
-        STATE_OF_EVENT_LIST.add(new TypeMapper<>(StateOfEvent.OFF, AASStateOfEvent.of(AASStateOfEvent.Options.off)));
+        //STATE_OF_EVENT_LIST = new ArrayList<>();
+        //STATE_OF_EVENT_LIST.add(new TypeMapper<>(StateOfEvent.ON, AASStateOfEvent.of(AASStateOfEvent.Options.on)));
+        //STATE_OF_EVENT_LIST.add(new TypeMapper<>(StateOfEvent.OFF, AASStateOfEvent.of(AASStateOfEvent.Options.off)));
 
         SUBMODEL_ELEMENTS_DATATYPE = new ArrayList<>();
         SUBMODEL_ELEMENTS_DATATYPE
